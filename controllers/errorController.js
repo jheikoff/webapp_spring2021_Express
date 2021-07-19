51e70a298e5d97a4ab5ca749204af4b769039353
jheikoff <60203909+jheikoff@users.noreply.github.com>
2021-07-18 21:54:38 -0600

    fixed error error

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -3,7 +3,7 @@ const httpStatus = require("http-status-codes")
 exports.pageNotFoundError = (req, res) =>{
     let errorCode = httpStatus.NOT_FOUND;
     res.status(errorCode);
-    res.render(error);
+    res.render("error");
 }
 
 exports.internalServerError = (error, req, res, next) => {
@@ -11,5 +11,4 @@ exports.internalServerError = (error, req, res, next) => {
     res.status(errorCode);
     res.send(`${errorCode}, Sorry! Something went wrong with your application`);
     console.error(error.stack)
-    next()
 }
